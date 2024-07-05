e20dfa089fb8f1641ac486758795ae335b4c8d47
Harutyacc <caoyifan9213@gmail.com>
2024-07-06 04:33:11 +0800

    githbu

--- tools/test.py
+++ tools/test.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     net_path = 'pretrained/siamfc_alexnet_e50.pth'
     tracker = TrackerSiamFC(net_path=net_path)
 
-    root_dir = os.path.expanduser('~/data/OTB/OTB100')
+    root_dir = os.path.expanduser('~/data/OTB2015/OTB100')
     e = ExperimentOTB(root_dir, version=2015)
     e.run(tracker, visualize=True)
     e.report([tracker.name])
